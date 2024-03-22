540b224d152f25c7792516d4f0d6134639a81f6d
/bin/bash -c "$(curl -fsSL https://gist.githubusercontent.com/stevebrownlee/b146bf49071c46c41eddf5778b147a71/raw/a1f8f3bebf7fab75e5bfb0d1ecabc89ab22baec8/verify-installs.sh) <christinam1215@yahoo.com>
2024-03-22 12:55:22 -0500

    fixed the repeating bills to show the original bill and the repeating instances

--- src/components/bills/BillList.jsx
+++ src/components/bills/BillList.jsx
@@ -17,23 +17,26 @@ export const BillList = ({ currentUser }) => {
 
   const getAndSetBills = () => {
     getAllBills().then((billsArray) => {
-      const repeatedBills = [];
+      const allBillsWithRepeats = [];
 
       billsArray.forEach((bill) => {
-        if (bill.repeatBillId) {
+        if (bill.repeatBillId > 1) {
           const newDueDates = calculateNewDueDate(
             bill.dueDate,
             bill.repeatBillId
           );
+
+          allBillsWithRepeats.push({ ...bill, dueDate: bill.dueDate });
+
           newDueDates.forEach((dueDate) => {
             const repeatedBillInstance = { ...bill, dueDate };
-            repeatedBills.push(repeatedBillInstance);
+            allBillsWithRepeats.push(repeatedBillInstance);
           });
         } else {
-          repeatedBills.push(bill);
+          allBillsWithRepeats.push(bill);
         }
       });
-      setAllBills(repeatedBills);
+      setAllBills(allBillsWithRepeats);
     });
   };
 
@@ -100,65 +103,61 @@ export const BillList = ({ currentUser }) => {
     const dueDates = [];
     const currentDate = new Date(dueDate);
 
-    if (repeatBillId === 1) {
-      dueDates.push(currentDate.toISOString().split("T")[0]);
-    } else {
-      for (let i = 0; i < 50; i++) {
-        switch (repeatBillId) {
-          case 2:
-            currentDate.setDate(currentDate.getDate() + 7);
-            break;
-          case 3:
-            currentDate.setDate(currentDate.getDate() + 14);
-            break;
-          case 4:
-            currentDate.setDate(currentDate.getDate() + 21);
-            break;
-          case 5:
-            currentDate.setDate(currentDate.getDate() + 28);
-            break;
-          case 6:
-            const dayOfMonth = currentDate.getDate();
-            if (dayOfMonth < 15) {
-              currentDate.setDate(15);
-            } else {
-              currentDate.setMonth(currentDate.getMonth() + 1);
-              currentDate.setDate(1);
-            }
-            break;
-          case 7:
+    for (let i = 0; i < 50; i++) {
+      switch (repeatBillId) {
+        case 2:
+          currentDate.setDate(currentDate.getDate() + 7);
+          break;
+        case 3:
+          currentDate.setDate(currentDate.getDate() + 14);
+          break;
+        case 4:
+          currentDate.setDate(currentDate.getDate() + 21);
+          break;
+        case 5:
+          currentDate.setDate(currentDate.getDate() + 28);
+          break;
+        case 6:
+          const dayOfMonth = currentDate.getDate();
+          if (dayOfMonth < 15) {
+            currentDate.setDate(15);
+          } else {
             currentDate.setMonth(currentDate.getMonth() + 1);
-            break;
-          case 8:
-            currentDate.setMonth(currentDate.getMonth() + 2);
-            break;
-          case 9:
-            currentDate.setMonth(currentDate.getMonth() + 3);
-            break;
-          case 10:
-            currentDate.setMonth(currentDate.getMonth() + 4);
-            break;
-          case 11:
-            currentDate.setMonth(currentDate.getMonth() + 6);
-            break;
-          case 12:
-            currentDate.setFullYear(currentDate.getFullYear() + 1);
-            break;
-          case 13:
-            currentDate.setFullYear(currentDate.getFullYear() + 2);
-            break;
-          case 14:
-            currentDate.setFullYear(currentDate.getFullYear() + 3);
-            break;
-          case 15:
-            currentDate.setFullYear(currentDate.getFullYear() + 4);
-            break;
-          case 16:
-            currentDate.setFullYear(currentDate.getFullYear() + 5);
-            break;
-        }
-        dueDates.push(currentDate.toISOString().split("T")[0]);
+            currentDate.setDate(1);
+          }
+          break;
+        case 7:
+          currentDate.setMonth(currentDate.getMonth() + 1);
+          break;
+        case 8:
+          currentDate.setMonth(currentDate.getMonth() + 2);
+          break;
+        case 9:
+          currentDate.setMonth(currentDate.getMonth() + 3);
+          break;
+        case 10:
+          currentDate.setMonth(currentDate.getMonth() + 4);
+          break;
+        case 11:
+          currentDate.setMonth(currentDate.getMonth() + 6);
+          break;
+        case 12:
+          currentDate.setFullYear(currentDate.getFullYear() + 1);
+          break;
+        case 13:
+          currentDate.setFullYear(currentDate.getFullYear() + 2);
+          break;
+        case 14:
+          currentDate.setFullYear(currentDate.getFullYear() + 3);
+          break;
+        case 15:
+          currentDate.setFullYear(currentDate.getFullYear() + 4);
+          break;
+        case 16:
+          currentDate.setFullYear(currentDate.getFullYear() + 5);
+          break;
       }
+      dueDates.push(currentDate.toISOString().split("T")[0]);
     }
     return dueDates;
   };
